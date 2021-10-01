3ce1efcf17d062938ef64bd9bf1c451b691a27a8
atingupta2005 <atingupta2005@gmail.com>
2021-10-01 18:27:01 +0530

    Update glue.tf

--- glue.tf
+++ glue.tf
@@ -37,7 +37,7 @@ resource "aws_glue_job" "aws_glue_job_nginx_logs_processing" {
   role_arn = aws_iam_role.aws_iam_glue_role.arn
 
   command {
-    script_location = "s3://${aws_s3_bucket.bucket_for_glue.bucket}/${aws_s3_bucket.bucket_for_glue.bucket.script.key}"
+    script_location = "s3://${aws_s3_bucket.bucket_for_glue.bucket}/${aws_s3_bucket.bucket_for_glue.script.key}"
   }
 
   default_arguments = {
